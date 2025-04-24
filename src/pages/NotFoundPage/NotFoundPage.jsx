ac613688c3eac3b2bdd8be2a0eeac1091dc86eab
Kateryna Troyan <ket.tro@gmail.com>
2025-04-24 18:54:24 +0300

    add NotFoundPage.module.css

--- src/pages/NotFoundPage/NotFoundPage.jsx
+++ src/pages/NotFoundPage/NotFoundPage.jsx
@@ -1,10 +1,13 @@
 import { Link } from 'react-router-dom';
+import css from './NotFoundPage.module.css';
 
 export default function NotFoundPage() {
   return (
     <>
-      <h1>404 Page not found</h1>
-      <Link to="/">Home</Link>
+      <h2 className={css.title}>404 Page not found</h2>
+      <Link to="/" className={css.link}>
+        Home
+      </Link>
     </>
   );
 }
